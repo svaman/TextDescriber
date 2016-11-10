77fc9a1ea2837d4f327bbe3143ae3ba5d2335154
Vishnu Aman <vishnu.aman@outlook.com>
2016-11-10 21:34:39 +1300

    Added console code to take input and print output

--- TextDescriber.Domain/PalindromeChecker.cs
+++ TextDescriber.Domain/PalindromeChecker.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TextDescriberDomainInterfaces;
+﻿using TextDescriberDomainInterfaces;
 
 namespace TextDescriberDomain
 {
--- TextDescriber/TextDescriber.cs
+++ TextDescriber/TextDescriber.cs
@@ -1,10 +1,43 @@
-﻿namespace TextDescriber
+﻿using System;
+using TextDescriberDomain;
+
+namespace TextDescriber
 {
     public class TextDescriber
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Press quit and press enter to quit");
+            var inputText = "";
+            while (true)
+            {
+                inputText = Console.ReadLine();
+                if (inputText == "quit")
+                {
+                    return;
+                }
+                var outputText = GetOutputValue(inputText);
+                Console.WriteLine(outputText);
+            }
+        }
+        
+        private static string GetOutputValue(string inputText)
+        {
+            var palindromeChecker = new PalindromeChecker();
+
+            if (palindromeChecker.IsPalindrome(inputText))
+            {
+                return "Palindrome";
+            }
+
+            var anagramPalindromeChecker = new AnagramPalindromeChecker();
+
+            if (anagramPalindromeChecker.IsAnagramPalindrome(inputText))
+            {
+                return "Anagram of a palindrome";
+            }
+
+            return "Neither";
         }
     }
 }
