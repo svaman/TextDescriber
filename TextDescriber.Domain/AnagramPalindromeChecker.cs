01e102825d6131a351a2c03c503b40ad8f8ff85e
Vishnu Aman <vishnu.aman@outlook.com>
2016-11-10 19:57:11 +1300

    Added AnagramPalindromeChecker and first unit test

---
+++ TextDescriber.Domain/AnagramPalindromeChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDescriberDomain
+{
+    public class AnagramPalindromeChecker
+    {
+        public bool IsAnagramPalindrome(string inputText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TextDescriberTests/AnagramPalindromeCheckerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TextDescriberDomain;
+
+namespace TextDescriberTests
+{
+    [TestFixture]
+    public class AnagramPalindromeCheckerTests
+    {
+
+        public void IsAnagramPalindrome_AllCharactersSame_ReturnsTrue()
+        {
+            //Arrange
+            var textDescriber = new AnagramPalindromeChecker();
+            var inputString = "aaaaaaaa";
+            //Act
+            var actualResult = textDescriber.IsAnagramPalindrome(inputString);
+            //Assert
+            Assert.IsTrue(actualResult);
+        }
+
+    }
+}
